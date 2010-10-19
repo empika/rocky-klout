305f7e8074607ca20a823848613cc91fb32033ef
Eddy Parris <eddy@communecation.net>
2010-10-19 22:26:18 +0100

    added the rest of the api functions and added some quick unit tests

--- lib/rocky-klout.rb
+++ lib/rocky-klout.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'json'
 require 'httparty'
+begin
+  require 'cgi'
+end
 
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
@@ -30,33 +33,51 @@ class RockyKlout
       @@api_key
     end
 
-    def score(usernames)
-      request_uri = "/#{@@api_version}/klout.json?key=#{@@api_key}&users=" + usernames.join(",")
+    def score_klout(usernames)
+      request_uri = "/#{@@api_version}/klout.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
       self.class.get(@@base_uri + request_uri)
     end
     
-    def users(usernames)
-      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&users=" + usernames.join(",")
+    def user_show(usernames)
+      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
       self.class.get(@@base_uri + request_uri)
     end  
     
-    def influenced_by(usernames)
-      request_uri = "/#{@@api_version}/soi/influenced_by.json?key=#{@@api_key}&users=" + usernames.join(",")
+    def user_topics(usernames)
+      request_uri = "/#{@@api_version}/users/topics.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
       self.class.get(@@base_uri + request_uri)
     end
     
-    def influcencer_of(usernames)
-      request_uri = "/#{@@api_version}/soi/influencer_of.json?key=#{@@api_key}&users=" + usernames.join(",")
+    def user_stats(usernames)
+      request_uri = "/#{@@api_version}/users/stats.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+      self.class.get(@@base_uri + request_uri)
+    end
+    
+    def user_history(usernames, measure, start_date, end_date)
+      request_uri = "/#{@@api_version}/users/show.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",") +
+        "&measure=" + CGI.escape(measure) +
+        "&start_date=" + CGI.escape(start_date)
+        "&end_date=" + CGI.escape(end_date)
+      self.class.get(@@base_uri + request_uri)
+    end
+    
+    def relationship_influenced_by(usernames)
+      request_uri = "/#{@@api_version}/soi/influenced_by.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
+      self.class.get(@@base_uri + request_uri)
+    end
+    
+    def relationship_influcencer_of(usernames)
+      request_uri = "/#{@@api_version}/soi/influencer_of.json?key=#{@@api_key}&users=" + usernames.collect{|name| CGI.escape(name)}.join(",")
       self.class.get(@@base_uri + request_uri)
     end
     
     def topic_search(topic)
-      request_uri = "/#{@@api_version}/topics/search.json?key=#{@@api_key}&users=" + CGI.escape(topic)
+      request_uri = "/#{@@api_version}/topics/search.json?key=#{@@api_key}&topic=" + CGI.escape(topic)
       self.class.get(@@base_uri + request_uri)
     end
     
-    def topic_verfiy(topic)
-      request_uri = "/#{@@api_version}/topics/verify.json?key=#{@@api_key}&users=" + CGI.escape(topic)
+    def topic_verify(topic)
+      request_uri = "/#{@@api_version}/topics/verify.json?key=#{@@api_key}&topic=" + CGI.escape(topic)
       self.class.get(@@base_uri + request_uri)
     end
     
--- test/test_rocky-klout.rb
+++ test/test_rocky-klout.rb
@@ -3,9 +3,122 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 class TestRockyKlout < Test::Unit::TestCase
 
   def setup
+    usage = "Usage: ruby test_rocky-klout.rb -- [klout_api_key]"
+    puts "Uh oh! \n"
+    puts usage
+    unless !ARGV[0].nil?
+      
+      exit
+    end
+    @api_key = ARGV[0] #"uxs4r9352tszfnau7np97zqs"
+    @rk = RockyKlout.new(@api_key)
+    @user_single = "darthvader"
+    @user_array = ["darthvader", "DeathStarPR"]
+    @measure = nil # not used yet, see test
+    @start_date = nil # not used yet, see test
+    @end_date = nil # not used yet, see test
+    @topic = "star wars"
   end
   
-  def test_truth
-    assert true
+  # klout score
+  def test_score_klout_single
+    response = @rk.score_klout(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_score_klout_array
+    response = @rk.score_klout(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  # user methods
+  def test_user_show_single
+    response = @rk.user_show(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_user_show_array
+    response = @rk.user_show(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_user_topics_single
+    response = @rk.user_topics(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_user_topics_array
+    response = @rk.user_topics(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_user_stats_single
+    response = @rk.user_stats(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_user_stats_array
+    response = @rk.user_stats(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  # skip these because we have no way of knowing what the arguments are
+  # def test_user_history_single
+  #   response = @rk.user_history(@user_single, @measure, @start_date, @end_date)
+  #   assert_instance_of Hash, response
+  #   assert_equal response["status"], 200
+  # end
+  # 
+  # def test_user_history_array
+  #   response = @rk.user_history(@user_array, @measure, @start_date, @end_date)
+  #   assert_instance_of Hash, response
+  #   assert_equal response["status"], 200
+  # end
+  
+  # relationship methods
+  def test_relationship_influenced_by_single
+    response = @rk.relationship_influenced_by(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_relationship_influenced_by_array
+    response = @rk.relationship_influenced_by(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_relationship_influcencer_of_single
+    response = @rk.relationship_influenced_by(@user_single)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_relationship_influcencer_of_array
+    response = @rk.relationship_influenced_by(@user_array)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  
+  # topic methods
+  def test_topic_search_single
+    response = @rk.topic_search(@topic)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
+  end
+  
+  def test_topic_verify_array
+    response = @rk.topic_verify(@topic)
+    assert_instance_of Hash, response
+    assert_equal response["status"], 200
   end
 end
